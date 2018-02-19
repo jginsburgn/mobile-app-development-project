db04fcb4df98ddeea7160271bc2c7f034f47e252
Jonathan Ginsburg <a01021617@itesm.mx>
2018-02-19 02:01:42 -0600

    Add project (180218) that merges: autocomplete, survey and shared preferences. Highlights: Fragment usage with bottom navigation bar of android.

---
+++ 180218/app/src/main/java/me/itsof/a180218/Autocomplete.java
@@ -0,0 +1,41 @@
+package me.itsof.a180218;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class Autocomplete extends Fragment {
+
+    private  String[] common = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"};
+
+    public Autocomplete() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_autocomplete, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getView().getContext(), android.R.layout.simple_dropdown_item_1line, common);
+        AutoCompleteTextView actv = (AutoCompleteTextView) getView().findViewById(R.id.autoComplete);
+
+        actv.setThreshold(1);
+        actv.setAdapter(adapter);
+    }
+
+}
---
+++ 180218/app/src/main/java/me/itsof/a180218/Main.java
@@ -0,0 +1,51 @@
+package me.itsof.a180218;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity {
+
+    private ScrollView container;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Autocomplete fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_autocomplete:
+                    fragment = new Autocomplete();
+                    break;
+                default:
+                    return false;
+            }
+            if (container.getChildCount() == 1) container.removeViewAt(0);
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.container, fragment);
+            fragmentTransaction.commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        container = (ScrollView) findViewById(R.id.container);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        navigation.setSelectedItemId(R.id.navigation_autocomplete);
+    }
+
+}
